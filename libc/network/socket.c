a0730d3ce72eecd9da3196962e8e2f7d33ffc5df
Guy Zana <guy@cloudius-systems.com>
2013-03-18 11:34:03 +0200

    socket: fix asserts of SOCK_CLOEXEC

--- libc/network/socket.c
+++ libc/network/socket.c
@@ -9,7 +9,7 @@ int socket(int domain, int type, int protocol)
     int s, error;
 
     /* This goes in release... */
-    assert(type & SOCK_CLOEXEC == 0);
+    assert(((type & SOCK_CLOEXEC) == 0));
 
     error = sys_socket(domain, (type & ~SOCK_NONBLOCK), protocol, &s);
     if (error) {
