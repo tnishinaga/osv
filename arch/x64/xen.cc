d4df83b11d17bd9e668a276b6676f0b1f287be79
Avi Kivity <avi@cloudius-systems.com>
2013-04-21 20:27:37 +0300

    xen: hypercalls
    Due to the wierd calling convention (induced by SYSCALL instruction
    constraints), we have an inline implementation for <=3 arguments,
    and an out-of-line implementation for 4-4 arguments.  The extra overhead
    is unlikely to matter since 4+ argument hypercalls will tend to be
    heavyweight anyway.

--- arch/x64/xen.cc
+++ arch/x64/xen.cc
@@ -3,6 +3,70 @@
 
 struct start_info* xen_start_info;
 extern void* xen_bootstrap_end;
+extern char xen_hypercall_page[];
+extern "C" { ulong xen_hypercall_5(ulong a1, ulong a2, ulong a3, ulong a4, ulong a5, unsigned type); }
+
+namespace xen {
+
+// we only have asm constraints for the first three hypercall args,
+// so we only inline hypercalls with <= 3 args.  The others are out-of-line,
+// implemented in normal asm.
+inline ulong hypercall(unsigned type, ulong a1, ulong a2, ulong a3)
+{
+    ulong ret;
+    asm volatile("call *%[hyp]"
+                 : "=a"(ret), "+D"(a1), "+S"(a2), "+d"(a3)
+                 : [hyp]"c"(xen_hypercall_page + 32 * type)
+                 : "memory", "r10", "r8");
+    return ret;
+}
+
+inline ulong hypercall(unsigned type, ulong a1, ulong a2, ulong a3, ulong a4, ulong a5)
+{
+    // swap argument order to get the first three register pre-loaded
+    return xen_hypercall_5(a1, a2, a3, a4, a5, type);
+}
+
+inline ulong hypercall(unsigned type)
+{
+    return hypercall(type, 0, 0, 0);
+}
+
+inline ulong hypercall(unsigned type, ulong a1)
+{
+    return hypercall(type, a1, 0, 0);
+}
+
+inline ulong hypercall(unsigned type, ulong a1, ulong a2)
+{
+    return hypercall(type, a1, a2, 0);
+}
+
+inline ulong hypercall(unsigned type, ulong a1, ulong a2, ulong a3, ulong a4)
+{
+    return hypercall(type, a1, a2, a3, a4, 0);
+}
+
+// some template magic to auto-cast pointers to ulongs in hypercall().
+
+inline ulong cast_pointer(ulong v)
+{
+    return v;
+}
+
+inline ulong cast_pointer(void* p)
+{
+    return reinterpret_cast<ulong>(p);
+}
+
+template <typename... T>
+inline ulong
+hypercall(unsigned type, T... args)
+{
+    return hypercall(type, cast_pointer(args)...);
+}
+
+}
 
 extern "C"
 void xen_init(struct start_info* si)
