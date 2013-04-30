8eab406f3789f8efaefd01670d317cd8dffe2c5f
Christoph Hellwig <hch@cloudius-systems.com>
2013-04-30 11:11:14 +0200

    more include order fallout

--- bsd/sys/cddl/compat/opensolaris/kern/opensolaris_cmn_err.c
+++ bsd/sys/cddl/compat/opensolaris/kern/opensolaris_cmn_err.c
@@ -27,6 +27,7 @@
 
 #include <sys/assfail.h>
 #include <sys/cmn_err.h>
+#include <stdio.h>
 
 void
 vcmn_err(int ce, const char *fmt, va_list adx)
--- bsd/sys/cddl/contrib/opensolaris/uts/common/sys/cmn_err.h
+++ bsd/sys/cddl/contrib/opensolaris/uts/common/sys/cmn_err.h
@@ -52,48 +52,38 @@ extern "C" {
 #ifdef _KERNEL
 
 /*PRINTFLIKE2*/
-extern void cmn_err(int, const char *, ...)
-    __KPRINTFLIKE(2);
+extern void cmn_err(int, const char *, ...);
 #pragma rarely_called(cmn_err)
 
-extern void vzcmn_err(zoneid_t, int, const char *, __va_list)
-    __KVPRINTFLIKE(3);
+extern void vzcmn_err(zoneid_t, int, const char *, va_list);
 #pragma rarely_called(vzcmn_err)
 
-extern void vcmn_err(int, const char *, __va_list)
-    __KVPRINTFLIKE(2);
+extern void vcmn_err(int, const char *, va_list);
 #pragma rarely_called(vcmn_err)
 
 /*PRINTFLIKE3*/
-extern void zcmn_err(zoneid_t, int, const char *, ...)
-    __KPRINTFLIKE(3);
+extern void zcmn_err(zoneid_t, int, const char *, ...);
 #pragma rarely_called(zcmn_err)
 
-extern void vzprintf(zoneid_t, const char *, __va_list)
-    __KVPRINTFLIKE(2);
+extern void vzprintf(zoneid_t, const char *, va_list);
 #pragma rarely_called(vzprintf)
 
 /*PRINTFLIKE2*/
-extern void zprintf(zoneid_t, const char *, ...)
-    __KPRINTFLIKE(2);
+extern void zprintf(zoneid_t, const char *, ...);
 #pragma rarely_called(zprintf)
 
 /*PRINTFLIKE1*/
-extern void uprintf(const char *, ...)
-    __KPRINTFLIKE(1);
+extern void uprintf(const char *, ...);
 #pragma rarely_called(uprintf)
 
-extern void vuprintf(const char *, __va_list)
-    __KVPRINTFLIKE(1);
+extern void vuprintf(const char *, va_list);
 #pragma rarely_called(vuprintf)
 
 /*PRINTFLIKE1*/
-extern void panic(const char *, ...)
-    __KPRINTFLIKE(1) __NORETURN;
+extern void panic(const char *, ...);
 #pragma rarely_called(panic)
 
-extern void vpanic(const char *, __va_list)
-    __KVPRINTFLIKE(1) __NORETURN;
+extern void vpanic(const char *, va_list);
 #pragma rarely_called(vpanic)
 
 #endif /* _KERNEL */
