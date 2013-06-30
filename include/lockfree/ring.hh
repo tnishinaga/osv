3c4412785e6640dba20544c5a442bf9a299646ea
Guy Zana <guy@cloudius-systems.com>
2013-06-30 14:40:57 +0300

    lockless: spsc ring buffer of fixed size
    single-producer / single-consumer lockless ring buffer of fixed size.
    
        1. _begin points to the head of the ring, _end points to the tail.
           both advance forward, adding items (_end++), consuming items (_begin++)
    
        2. all indexes of the ring are running, so the condition for empty ring is
           different than full ring.
                *) Empty -> (_begin == _end)
                *) Full -> (_end - _begin = MaxSize)
    
        3. uses only store and load operations, no rmw.
    
        4. a waiting version (wait_ring_spsc) with a blocking push()
           is provided as well.

---
+++ include/lockfree/ring.hh
@@ -0,0 +1,93 @@
+//
+// single-producer / single-consumer lockless ring buffer of fixed size.
+//
+#ifndef __LF_RING_HH__
+#define __LF_RING_HH__
+
+#include <atomic>
+#include <sched.hh>
+
+#define CACHELINE_ALIGNED __attribute__((aligned(64)))
+
+//
+// spsc ring of fixed size
+//
+template<class T, unsigned MaxSize>
+class ring_spsc {
+public:
+    ring_spsc(): _begin(0), _end(0) { }
+
+    bool push(const T& element)
+    {
+        unsigned end = _end.load(std::memory_order_relaxed);
+        unsigned beg = _begin.load(std::memory_order_relaxed);
+
+        if (end - beg >= MaxSize) {
+            return false;
+        }
+
+        _ring[end % MaxSize] = element;
+        _end.store(end + 1, std::memory_order_release);
+
+        return true;
+    }
+
+    bool pop(T& element)
+    {
+        unsigned beg = _begin.load(std::memory_order_relaxed);
+        unsigned end = _end.load(std::memory_order_acquire);
+
+        if (beg >= end) {
+            return false;
+        }
+
+        element = _ring[beg % MaxSize];
+        _begin.store(beg + 1, std::memory_order_relaxed);
+
+        return true;
+    }
+
+private:
+    std::atomic<unsigned> _begin CACHELINE_ALIGNED;
+    std::atomic<unsigned> _end CACHELINE_ALIGNED;
+    T _ring[MaxSize];
+};
+
+//
+// A spsc ring that has a blocking push()
+//
+template<class T, unsigned MaxSize>
+class wait_ring_spsc {
+public:
+
+    wait_ring_spsc(): _ring(), _waiter(nullptr) { }
+
+    void push(const T& element)
+    {
+        if (!_ring.push(element)) {
+            _waiter.store(sched::thread::current(), std::memory_order_relaxed);
+            sched::thread::wait_until([&] { return (_ring.push(element)); });
+            _waiter.store(nullptr, std::memory_order_relaxed);
+        }
+    }
+
+    bool pop(T& element)
+    {
+        bool rc = _ring.pop(element);
+        if (rc) {
+            sched::thread* waiter = _waiter.load(std::memory_order_relaxed);
+            if (waiter) {
+                waiter->wake();
+            }
+        }
+
+        return rc;
+    }
+
+private:
+    ring_spsc<T, MaxSize> _ring;
+    std::atomic<sched::thread*> _waiter CACHELINE_ALIGNED;
+};
+
+
+#endif // !__LF_RING_HH__
