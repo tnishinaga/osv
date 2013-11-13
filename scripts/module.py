fcf929c2f8695718fdbef9cf4fe1267949893be5
Takuya ASADA <syuu@dokukino.com>
2013-11-13 14:30:11 -0800

    introduce property file for each module

--- scripts/module.py
+++ scripts/module.py
@@ -4,6 +4,9 @@ import json
 import sys
 import os
 import subprocess
+import hashlib
+
+properties_version = 1
 
 f = open("../../../config.json")
 conf = json.load(f)
@@ -16,27 +19,47 @@ for l in mskel.readlines():
 	m.write(l)
 mskel.close()
 
+if os.path.exists("tmp") == True:
+	os.rmdir("tmp")
+
+mprops = {}
 for mod in conf["modules"]:
-	mmod_path = "%s/%s.manifest" % (mod["name"], mtype)
-	if os.access(mod["name"], os.F_OK) == False:
+	md5 = hashlib.md5()
+	md5.update("%s" % mod["path"])
+	mprop_path = "%s.properties" % md5.hexdigest()
+	if os.path.exists(mprop_path) == False:
 		if mod["type"] == "git":
-			cmd = "git clone -b %s %s %s" % (mod["branch"], mod["path"], mod["name"])
+			cmd = "git clone -b %s %s tmp" % (mod["branch"], mod["path"])
 		elif mod["type"] == "svn":
-			cmd = "svn co %s %s" % (mod["path"], mod["name"])
+			cmd = "svn co %s tmp" % (mod["path"])
 		elif mod["type"] == "dir":
-			cmd = "cp -a %s %s" % (mod["path"], mod["name"])
+			cmd = "cp -a %s tmp" % (mod["path"])
 		else:
 			raise Exception("%s is unknown type" % mod["type"])
 		print cmd
 		subprocess.call([cmd], shell=True)
-	if os.access(mmod_path, os.F_OK) == False:
-		pwd = os.getcwd()
-		print "cd %s" % mod["name"]
-		os.chdir(mod["name"])
+		f = open("tmp/properties.json")
+		mprop = json.load(f)
+		f.close()
+		os.rename("tmp", mprop["name"])
+		os.symlink("%s/properties.json" % mprop["name"], mprop_path)
+	else:
+		f = open(mprop_path)
+		mprop = json.load(f)
+		f.close()
+	mprops[mprop["name"]] = mprop
+
+for mprop in mprops.values():
+	mmod_path = "%s/%s.manifest" % (mprop["name"], mtype)
+	if os.path.exists(mmod_path) == False:
+		if mprop["properties_version"] != properties_version:
+			raise Exception("[%s] unsupported properties version" % mprop["name"])
+		for dep in mprop["dependencies"]:
+			if mprop.has_key(dep) == False:
+				raise Exception("[%s] requires %s" % (mprop["name"], dep))
 		cmd = "make module"
-		subprocess.call([cmd], shell=True)
-		print "cd -"
-		os.chdir(pwd)
+		print cmd
+		subprocess.call([cmd], cwd=mprop["name"], shell=True)
 	print "append %s to %s.manifest" % (mmod_path, mtype)
 	mmod = open(mmod_path)
 	for l in mmod.readlines():
