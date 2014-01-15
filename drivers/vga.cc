ab65a70eabd246d96fd32ca86fd0c55f95fab9ed
Takuya ASADA <syuu@cloudius-systems.com>
2014-01-15 16:43:51 +0900

    Handle all input/output by libtsm

--- drivers/vga.cc
+++ drivers/vga.cc
@@ -7,50 +7,112 @@
 
 #include "vga.hh"
 #include "mmu.hh"
+#include "libtsm/external/xkbcommon-keysyms.h"
 
 volatile unsigned short * const VGAConsole::buffer
 = reinterpret_cast<volatile unsigned short *>(mmu::phys_mem + 0xb8000);
 
-VGAConsole::VGAConsole(sched::thread* poll_thread, const termios *tio)
-    : _tio(tio)
+static void tsm_log_cb(void *data, const char *file, int line, const char *func,
+    const char *subs, unsigned int sev, const char *format, va_list args)
 {
+    printf("%s:%d:%s subs:%s sev:%d ", file, line, func, subs, sev);
+    vprintf(format, args);
+    printf("\n");
+}
+
+static void tsm_write_cb(struct tsm_vte *vte, const char *u8, size_t len,
+    void *data)
+{
+    VGAConsole *vga = reinterpret_cast<VGAConsole *>(data);
+    for (size_t i = 0; i < len; i++)
+        vga->read_queue.push(u8[i]);
+}
+
+static int tsm_draw_cb(struct tsm_screen *screen, uint32_t id,
+    const uint32_t *ch, size_t len, unsigned int cwidth, unsigned int posx,
+    unsigned int posy, const struct tsm_screen_attr *attr, tsm_age_t age,
+    void *data)
+{
+    VGAConsole *vga = reinterpret_cast<VGAConsole *>(data);
+
+    if (len)
+        vga->draw(*ch, attr, posx, posy);
+    else
+        vga->draw(' ', attr, posx, posy);
+    return 0;
+}
+
+VGAConsole::VGAConsole(sched::thread* poll_thread, const termios *tio)
+    : tsm_age(0), _tio(tio), kbd(poll_thread)
+{
+    tsm_screen_new(&tsm_screen, tsm_log_cb, this);
+    tsm_screen_resize(tsm_screen, ncols, nrows);
+    tsm_screen_set_max_sb(tsm_screen, 1024);
+    tsm_vte_new(&tsm_vte, tsm_screen, tsm_write_cb, this, tsm_log_cb, this);
 }
 
 void VGAConsole::write(const char *str, size_t len)
 {
-    while (len > 0) {
-        if ((*str == '\n') && (_tio->c_oflag & OPOST)
-            && (_tio->c_oflag & ONLCR))
-            newline();
-        else {
-            buffer[(nrows-1)*ncols + _col++] = 0x700 + *str;
-            if (_col == ncols)
-                newline();
-        }
-        str++;
-        len--;
-    }
+    tsm_vte_input(tsm_vte, str, len);
+    tsm_age = tsm_screen_draw(tsm_screen, tsm_draw_cb, this);
 }
 
-void VGAConsole::newline()
+void VGAConsole::draw(const uint32_t c, const struct tsm_screen_attr *attr, 
+    unsigned int x, unsigned int y)
 {
-    for (unsigned row = 0; row < nrows - 1; ++row) {
-	for (unsigned col = 0; col < ncols; ++col) {
-	    buffer[row*ncols+col] = buffer[(row+1)*ncols+col];
-	}
-    }
-    for (unsigned col = 0; col < ncols; ++col) {
-	buffer[(nrows - 1) * ncols + col] = 0x700;
-    }
-    _col = 0;
+    uint32_t colors = (attr->bccode << 4) | (attr->fccode & 0xf);
+    buffer[y * ncols + x] = (colors << 8) | (c & 0xff);
 }
 
 bool VGAConsole::input_ready()
 {
-    return false;
+    return !read_queue.empty() || kbd.input_ready();
 }
 
 char VGAConsole::readch()
 {
-    return 0;
+    uint32_t key;
+    unsigned int mods = 0;
+    char c;
+
+    while(1) {
+        if (!read_queue.empty()) {
+            c = read_queue.front();
+            read_queue.pop();
+            return c;
+        }
+
+        key = kbd.readkey();
+
+        if (kbd.shift & SHIFT)
+            mods |= TSM_SHIFT_MASK;
+        if (kbd.shift & CTL)
+            mods |= TSM_CONTROL_MASK;
+        if (kbd.shift & ALT)
+            mods |= TSM_ALT_MASK;
+
+        switch (key) {
+        case XKB_KEY_Up:
+            tsm_screen_sb_up(tsm_screen, 1);
+            tsm_age = tsm_screen_draw(tsm_screen, tsm_draw_cb, this);
+            break;
+        case XKB_KEY_Down:
+            tsm_screen_sb_down(tsm_screen, 1);
+            tsm_age = tsm_screen_draw(tsm_screen, tsm_draw_cb, this);
+            break;
+        case XKB_KEY_Page_Up:
+            tsm_screen_sb_page_up(tsm_screen, 1);
+            tsm_age = tsm_screen_draw(tsm_screen, tsm_draw_cb, this);
+            break;
+        case XKB_KEY_Page_Down:
+            tsm_screen_sb_page_down(tsm_screen, 1);
+            tsm_age = tsm_screen_draw(tsm_screen, tsm_draw_cb, this);
+            break;
+        default:
+            if (tsm_vte_handle_keyboard(tsm_vte, key, 0, mods, key))
+                tsm_screen_sb_reset(tsm_screen);
+        }
+        while (read_queue.empty())
+            ;
+    }
 }
--- drivers/vga.hh
+++ drivers/vga.hh
@@ -10,20 +10,28 @@
 
 #include "console.hh"
 #include "sched.hh"
+#include "kbd.hh"
+#include "libtsm/libtsm.h"
 #include <termios.h>
+#include <queue>
 
 class VGAConsole : public Console {
 public:
     explicit VGAConsole(sched::thread* consumer, const termios *tio);
     virtual void write(const char *str, size_t len);
-    virtual void newline();
     virtual bool input_ready();
     virtual char readch();
+    virtual void draw(const uint32_t c, const struct tsm_screen_attr *attr, unsigned int x, unsigned int y);
+    struct tsm_screen *tsm_screen;
+    struct tsm_vte *tsm_vte;
+    std::queue<char> read_queue;
+    tsm_age_t tsm_age;
 private:
     unsigned _col = 0;
     static const unsigned ncols = 80, nrows = 25;
     static volatile unsigned short * const buffer;
     const termios *_tio;
+    Keyboard kbd;
 };
 
 #endif
